22a1186e7a90fd662b980265bf468d79f9058f17
Mateus Pereira <mbpereira98@gmail.com>
2022-10-18 09:48:21 -0300

    ajustando consulta

--- lab-dotnet/PortExhaust/Controllers/CepController.cs
+++ lab-dotnet/PortExhaust/Controllers/CepController.cs
@@ -8,23 +8,28 @@ namespace PortExhaust.Controllers
     {
         private readonly ILogger<CepController> _logger;
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string _fakeApiBaseUrl;
+        private readonly string _fakeApiToken;
 
-        public CepController(ILogger<CepController> logger, IHttpClientFactory httpClientFactory)
+        public CepController(ILogger<CepController> logger, IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
             _logger = logger;
             _httpClientFactory = httpClientFactory;
+            _fakeApiBaseUrl = configuration.GetValue<string>("FAKE_API_BASE_URL");
+            _fakeApiToken = configuration.GetValue<string>("FAKE_API_TOKEN");
         }
 
-        [HttpGet("{cep}")]
-        public async Task<IActionResult> Get(string cep)
+        [HttpGet]
+        public async Task<IActionResult> Get()
         {
-            using var _ = _logger.BeginScope(new Dictionary<string, object> { { "cep", cep } });
+            using var _ = _logger.BeginScope(new Dictionary<string, object> { { "description", "instance-per-request" } });
 
             using (var httpClient = new HttpClient())
             {
+                httpClient.DefaultRequestHeaders.Add("x-token", _fakeApiToken);
                 _logger.LogInformation("Starting viacep");
 
-                var response = await httpClient.GetAsync($"https://viacep.com.br/ws/{cep}/json/");
+                var response = await httpClient.GetAsync($"{_fakeApiBaseUrl}/ceps");
                 var content = await response.Content.ReadAsStringAsync();
 
                 _logger.LogInformation($"viacep response {content}");
@@ -33,15 +38,17 @@ namespace PortExhaust.Controllers
             }
         }
 
-        [HttpGet("2/{cep}")]
-        public async Task<IActionResult> Get2(string cep)
+        [HttpGet("2")]
+        public async Task<IActionResult> Get2()
         {
-            using var _ = _logger.BeginScope(new Dictionary<string, object> { { "cep", cep } });
+            using var _ = _logger.BeginScope(new Dictionary<string, object> { {  "description", "single-instance" } });
 
             var httpClient = _httpClientFactory.CreateClient();
+            httpClient.DefaultRequestHeaders.Add("x-token", _fakeApiToken);
+
             _logger.LogInformation("Starting viacep");
 
-            var response = await httpClient.GetAsync($"https://viacep.com.br/ws/{cep}/json/");
+            var response = await httpClient.GetAsync($"{_fakeApiBaseUrl}/ceps");
             var content = await response.Content.ReadAsStringAsync();
 
             _logger.LogInformation($"viacep response {content}");
